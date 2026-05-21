c6e04c3971f4ba16de8269c5b598f1b81ec27451
Abdhullah-Al-Maruf <mdmaruf20053@gmail.com>
2026-05-21 11:18:16 +0600

    added post api to insert book apoointment data to appointments collection

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb'); 
-const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
 const app = express();
-const cors = require("cors"); 
-const port = 5000; 
+const cors = require("cors");
+const port = 5000;
 
-
-app.use(cors()); 
-app.use(express.json());  // this will convert the client data to json
-
+app.use(cors());
+app.use(express.json()); // this will convert the client data to json
 
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_SECRET}@cluster0.gklgo91.mongodb.net/?appName=Cluster0`;
 
@@ -18,55 +16,61 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
-await client.connect();
+    await client.connect();
     const db = client.db("doctor-appointment-data");
-    const doctorsCollection=db.collection("doctors")
+    const doctorsCollection = db.collection("doctors");
+    const appointmentsCollection = db.collection("appointments");
+
+    //  get api for all doctors data
+    app.get("/doctors", async (req, res) => {
+      const doctors = doctorsCollection.find({});
+      const result = await doctors.toArray();
+      res.send(result);
+    });
 
 
+    // api for single doctor data
+    app.get("/doctors/:id", async (req, res) => {
+      // get the id from the client
+      const id = req.params.id;
+      // query for the id
+      const query = {
+        _id: new ObjectId(id),
+      };
+
+      // find the doctor by id
+      const doctor = await doctorsCollection.findOne(query);
+      res.send(doctor);
+    });
 
 
-//  get api for all doctors data
-app.get("/doctors", async (req, res) => {
-  const doctors= doctorsCollection.find({});
-  const result =await doctors.toArray();  
-  res.send(result)
-
-})
-
-// api for single doctor data
-app.get("/doctors/:id",async(req,res)=>{
-  // get the id from the client
-  const id =req.params.id;
-// query for the id
-const query={
-  _id: new ObjectId(id),
-}
-// find the doctor by id
-const doctor= await doctorsCollection.findOne(query);
-res.send(doctor)
-})
-    
+    //  post api for add appointment data
+    app.post("/appointments",async(req,res)=>{
+      const appointment =req.body;
+      const result =await appointmentsCollection.insertOne(appointment);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
 
 run().catch(console.dir);
